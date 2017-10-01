5761e5d8513f4beffe73e5057d82ce86ad291203
Darren Nesbitt <darrencnesbitt@gmail.com>
2017-10-01 17:23:59 -0400

    Had to drop DB, create and re-seed tables

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :ingredients, only: [:index, :show]
+
+  resources :recipe_ingredients, only: [:index, :show]
+
+  resources :courses, only: [:index, :show] do
+    resources :recipes, only: [:index, :show]
+  end
 end
